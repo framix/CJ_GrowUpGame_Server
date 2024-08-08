68aa0c91e8873d826edf58123f12b0b44be3d23d
qoo_serface <lorynga@gmail.com>
2024-08-09 00:51:14 +0900

    운영서버 세팅값 반영

---
+++ db.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [{
+    name: 'sf_db',
+    script: './sf_db/run_db.js',
+    kill_timeout: 10000,
+    exec_mode: "fork",
+    instances: 1,  // 인스턴스 수 (포크 모드에서는 1로 설정)
+    autorestart: true,
+    max_memory_restart: '1G',
+  }],
+
+  deploy: {
+    production: {
+      user: 'ec2-user',
+      host: '3.35.70.185',
+      ref: 'origin/master',
+      repo: 'git@github.com:framix/CJ_GrowUpGame_Server.git',
+      path: '/home/master/CJ_GrowUpGame_Server',
+      'post-deploy': 'npm install && pm2 reload db.config.js --env production',
+    }
+  }
+};
---
+++ game.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [{
+    name: 'sf_game',
+    script: './sf_game/run_game.js',
+    kill_timeout: 10000,
+    instances: -1,
+    exec_mode: "cluster",
+    autorestart: true,
+    max_memory_restart: '1G',
+  }],
+
+  deploy: {
+    production: {
+      user: 'ec2-user',
+      host: '3.35.70.185',
+      ref: 'origin/master',
+      repo: 'git@github.com:framix/CJ_GrowUpGame_Server.git',
+      path: '/home/master/CJ_GrowUpGame_Server',
+      'post-deploy': 'npm install && pm2 reload game.config.js --env production',
+    }
+  }
+};
